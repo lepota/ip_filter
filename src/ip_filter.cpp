2b65d56a4121436d477ca23733f3edf759d69e00
Alexey Guschin <timelexa@gmail.com>
2020-06-14 14:26:26 +0300

    Add solution
    Change CMakeLists

--- src/ip_filter.cpp
+++ src/ip_filter.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -10,7 +11,7 @@
 // ("11.", '.') -> ["11", ""]
 // (".11", '.') -> ["", "11"]
 // ("11.22", '.') -> ["11", "22"]
-std::vector<std::string> split(const std::string &str, char d)
+auto split(const std::string &str, char d)
 {
     std::vector<std::string> r;
 
@@ -29,33 +30,99 @@ std::vector<std::string> split(const std::string &str, char d)
     return r;
 }
 
+using IP = std::vector<int>;
+
+auto to_int(std::vector<std::string>&& ip){
+    IP result;
+    for(const auto& item : ip)
+        result.push_back(std::stoi(item));
+    return result;
+}
+
+std::ostream& operator<<(std::ostream& stream, const IP& ip){
+    for(auto i = 0u; i < ip.size(); ++i){
+        stream << ip.at(i);
+        if(i != ip.size() - 1) stream << '.';
+    }
+    return stream;
+}
+
+using IP_POOL = std::vector<IP>;
+
+std::ostream& operator << (std::ostream& stream, const IP_POOL& ip_pool){
+    for(const auto& ip : ip_pool)
+        stream << ip << '\n';
+    return stream;
+}
+
+
+IP ip_address(){
+    return IP(0);
+}
+
+template<typename T, typename ... Args>
+auto ip_address(T number, Args ... args){
+    IP result = ip_address(args...);
+    result.push_back(number);
+    std::reverse(result.begin(), result.end());
+    return result;
+}
+
+void ip_filter(const IP_POOL& ip_pool, const IP& ip){
+    std::for_each(
+        ip_pool.begin(),
+        ip_pool.end(),
+        [=](auto a) {
+            int i = 0;
+            for(auto item : ip)
+                if (item != a.at(i)) return;
+                else ++i;
+            std::cout << a << '\n';
+        }
+    );
+}
+
+void ip_filter_any(const IP_POOL& ip_pool, int number){
+    std::for_each(
+        ip_pool.begin(),
+        ip_pool.end(),
+        [=](auto a){
+            for(auto item : a)
+                if (item == number){
+                    std::cout << a << '\n';
+                    return;
+                }
+        }
+    );
+}
+
 int main(/*int argc, char const *argv[]*/)
 {
     try
     {
-        std::vector<std::vector<std::string>> ip_pool;
+        std::vector<IP> ip_pool;
 
-        for(std::string line; std::getline(std::cin, line);)
-        {
-            std::vector<std::string> v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
+        for(std::string line; std::getline(std::cin, line);){
+            auto v = split(line, '\t');
+            ip_pool.push_back(to_int(split(v.at(0), '.')));
         }
 
         // TODO reverse lexicographically sort
+        std::sort(ip_pool.begin(), ip_pool.end(), [](auto a, auto b) { return (a > b); });
 
-        for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
-        {
-            for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
-            {
-                if (ip_part != ip->cbegin())
-                {
-                    std::cout << ".";
+        std::cout << ip_pool;
+        // for (auto ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip) {
+        //     for(auto ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
+        //     {
+        //         if (ip_part != ip->cbegin())
+        //         {
+        //             std::cout << ".";
 
-                }
-                std::cout << *ip_part;
-            }
-            std::cout << std::endl;
-        }
+        //         }
+        //         std::cout << *ip_part;
+        //     }
+        //     std::cout << std::endl;
+        // }
 
         // 222.173.235.246
         // 222.130.177.64
@@ -67,7 +134,7 @@ int main(/*int argc, char const *argv[]*/)
 
         // TODO filter by first byte and output
         // ip = filter(1)
-
+        ip_filter(ip_pool, ip_address(1));
         // 1.231.69.33
         // 1.87.203.225
         // 1.70.44.170
@@ -76,6 +143,7 @@ int main(/*int argc, char const *argv[]*/)
 
         // TODO filter by first and second bytes and output
         // ip = filter(46, 70)
+        ip_filter(ip_pool, ip_address(46, 70));
 
         // 46.70.225.39
         // 46.70.147.26
@@ -84,6 +152,7 @@ int main(/*int argc, char const *argv[]*/)
 
         // TODO filter by any byte and output
         // ip = filter_any(46)
+        ip_filter_any(ip_pool, 46);
 
         // 186.204.34.46
         // 186.46.222.194
